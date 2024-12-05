87f057ca716a40c2a268156cd32ffd1b5c9b4d1f
Aymeric Barthe <aymeric@barthe.ph>
2024-12-05 08:00:40 +0000

    Part 2.

--- day_05/README.md
+++ day_05/README.md
@@ -2,5 +2,19 @@
 
 ```
 deno task solve
-deno task solve
 ```
+
+## Issue
+
+The main issue in part 2 which is not apparent in the sample, is that **more than one rule** may be broken by a given input.
+
+I initially thought there would similar to the sample, where only one rule is breached… Took me some time to figure out that it was not the case.
+
+My way to fix it is to basically swap every two numbers that are in breach a rule until there are no breaches left.
+
+I suspect that's very inefficient. That's a bit like a bubble sort. Complexity must be O^2, or worse.
+
+It feels like a sort problem. And my implementation feels like a bubble sort implementation. This is what is in `solve.ts`
+
+So, ideally, one would like to deploy the quick sort algorithm from JavaScript and use `.sort()` but the problem is that it's unclear how to order two values `a` and `b`. 
+
--- day_05/solve.ts
+++ day_05/solve.ts
@@ -42,59 +42,25 @@ console.log("Part 1:", sum);
 
 // Part 2
 const unmatchUpdates = updates.filter(update => !matchUpdates.includes(update));
-console.log(unmatchUpdates);
+// console.log(unmatchUpdates);
 
-const indices = unmatchUpdates.map(update => {
-    const rule = rules.find(rule => !matchRule(rule, update))!;
-    const [before, after] = rule;
-    const beforeIndex = update.indexOf(before);
-    const afterIndex = update.indexOf(after);
-    return [beforeIndex, afterIndex];
-});
-// console.log(indices);
 
-function stripUpdate(update: number[], indices: number[]) {
-    const newupdate: (number | null)[] = [...update];
-    for (const index of indices) {
-        newupdate[index] = null;
+function fixUpdate(update: number[]): number[] {
+    const matchAllRules = (update: number[]) => rules.every(rule => matchRule(rule, update));
+    const candidate = [...update];
+    while (!matchAllRules(candidate)) {
+        const rule = rules.find(rule => !matchRule(rule, candidate))!;
+        const [before, after] = rule;
+        const beforeIndex = candidate.indexOf(before);
+        const afterIndex = candidate.indexOf(after);
+        const temp = candidate[beforeIndex];
+        candidate[beforeIndex] = candidate[afterIndex];
+        candidate[afterIndex] = temp;
     }
-    return newupdate.filter(x => x !== null) as number[];
+    return candidate;
 }
 
-const fixedUpdates = unmatchUpdates.map((update, i) => {
-    const a = update[indices[i][0]];
-    const b = update[indices[i][1]];
+const fixedUpdates = unmatchUpdates.map(fixUpdate);
+const sum2 = fixedUpdates.reduce((acc, update) => acc + update![(update!.length - 1) / 2], 0);
+console.log("Part 2:", sum2);
 
-    update = stripUpdate(update, [indices[i][0], indices[i][1]]);
-    const show = i === 0;
-
-    
-    // Brute force search for the correct order (ugly but works… there must be a better way)
-    for (let x = 0; x < update.length+1; x++) {
-        for (let y = 0; y < update.length+2; y++) {
-            const newUpdate = [...update];
-            newUpdate.splice(x, 0, a);
-            newUpdate.splice(y, 0, b);
-            if (show) {
-                console.log(newUpdate);
-            }
-            if (rules.every(rule => matchRule(rule, newUpdate))) {
-                return newUpdate;
-            }
-        }
-    }
-
-    // Non reachable
-    return null;
-});
-console.log(fixedUpdates);
-
-// const sum2 = fixedUpdates.reduce((acc, update) => acc + update[(update.length - 1) / 2], 0);
-// console.log("Part 2:", sum2);
-console.log(indices[0]);
-console.log(unmatchUpdates[0]);
-
-// [
-//     26, 11, 53, 72, 61,
-//     92, 17, 77, 37
-//   ],
