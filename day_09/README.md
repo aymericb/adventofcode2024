ee95322bdaad88f7aa0ed828aa18c7678f79be25
Aymeric Barthe <aymeric@barthe.ph>
2024-12-09 22:59:23 +0000

    Day 8 part 2 ugly method.

--- day_09/README.md
+++ day_09/README.md
@@ -4,3 +4,8 @@
 deno task solve
 ```
 
+Not my best work. I tried to make an algorithm working on the run length encoding principles, but gave up on it. Pretty sure I could make it work, but have some kind of fundamental mistake. 
+
+Anyhow, that's very ugly inefficient code, but it works.
+
+
--- day_09/solve.ts
+++ day_09/solve.ts
@@ -2,27 +2,25 @@ const text = await Deno.readTextFile("input.txt");
 // const text = await Deno.readTextFile("sample.txt");
 
 
-type Node = {
-    file: number | null;
-}
+type BitmapAlloc = (number | null)[];
 
-function parseData(text: string): Node[] {
+function parseData(text: string): BitmapAlloc {
     const indices = text.split("").map(Number);
-    const data: Node[] = [];
+    const data: BitmapAlloc = [];
     let in_file = true;
     let file = 0;
     for (const value of indices) {
         if (in_file) {
             const files = Array(value);
             for (let i=0; i<files.length; i++) {
-                files[i] = { file };
+                files[i] = file;
             }
             data.push(...files);
             file += 1;
         } else {
             const gaps = Array(value);
             for (let i=0; i<gaps.length; i++) {
-                gaps[i] = { file: null };
+                gaps[i] = null;
             }
             data.push(...gaps);
         }
@@ -31,32 +29,111 @@ function parseData(text: string): Node[] {
     return data;
 }
 
-function print(data: Node[]) {
-    console.log(data.map(x => x.file !== null ? x.file : ".").join(""));
+function print(data: BitmapAlloc) {
+    console.log(data.map(x => x !== null ? x : ".").join(""));
 }
 
-function compress(data: Node[]) {
+function compress(data: BitmapAlloc) {
     let j = data.length - 1;
     for (let i=0; i<data.length; i++) {
         // print(data);
-        if (data[i].file === null) {
-            while (data[j].file === null) {
+        if (data[i] === null) {
+            while (data[j] === null) {
                 j -= 1;
             }
             if (j <= i) {
                 return;
             }
-            data[i].file = data[j].file;
-            data[j].file = null;
+            data[i] = data[j];
+            data[j] = null;
             j -= 1;
         }
     }    
 }
 
-const data = parseData(text);
-// print(data);
-compress(data);
-// print(data);
+type Segment = {
+    start: number;
+    length: number;
+    value: number | null;
+}
 
-const part1 = data.reduce((acc, x, i) => acc + (x.file ?? 0) * i, 0);
+function findForwardSegment(data: BitmapAlloc, start: number): Segment  {
+    const file = data[start];
+    if (file !== null) {
+        throw new Error("Logic error, file is not null");
+    }
+    let i = start;
+    while (i < data.length && data[i] === file) {
+        i += 1;
+    }
+    return { start, length: i - start, value: file };
+}
+
+function findBackwardSegment(data: BitmapAlloc, start: number): Segment  {
+    const file = data[start];
+    if (file === null) {
+        throw new Error("Logic error, file is null");
+    }
+    let i = start;
+    while (i >= 0 && data[i] === file) {
+        i -= 1;
+    }
+    return { start: i + 1, length: start - i, value: file };
+}
+
+function fillSegment(data: BitmapAlloc, segment: Segment, file: number | null) {
+    for (let i=0; i<segment.length; i++) {
+        data[segment.start + i] = file;
+    }
+}
+
+function compressNofragment(data: BitmapAlloc) {
+
+    for (let j=data.length - 1; j>=0; j--) {
+        // console.log("j", j, data[j]);
+        // print(data);
+        if (data[j] === null) {
+            continue;
+        }
+        const file_segment = findBackwardSegment(data, j);
+        // console.log("file_segment", file_segment);
+
+        let skip = true;
+        for (let i=0; i<file_segment.start; i++) {
+            if (data[i] !== null) {
+                continue;
+            }
+            const free_segment = findForwardSegment(data, i);
+            // console.log("free_segment", free_segment);
+            if (free_segment.length >= file_segment.length) {
+                free_segment.length = file_segment.length;
+                fillSegment(data, free_segment, file_segment.value);
+                fillSegment(data, file_segment, null);
+                skip = false;
+                break;
+            }
+        }
+        if (skip) {
+            j -= file_segment.length;
+            j += 1;
+        }
+    }
+}
+
+
+const data = parseData(text);
+
+const dataPart1 = [...data];
+// print(dataPart1);
+compress(dataPart1);
+// print(dataPart1);
+
+const part1 = dataPart1.reduce((acc, x, i) => acc! + (x ?? 0) * i, 0);
 console.log("Part 1:", part1);
+
+const dataPart2 = [...data];
+// print(dataPart2);
+compressNofragment(dataPart2);
+// print(dataPart2);
+const part2 = dataPart2.reduce((acc, x, i) => acc! + (x ?? 0) * i, 0);
+console.log("Part 2:", part2);
