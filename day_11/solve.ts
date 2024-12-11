740c4b60974d4c66025a006fd0f225423ed3e8c0
Aymeric Barthe <aymeric@barthe.ph>
2024-12-11 06:37:12 +0000

    Part 2. Not enough RAM.

--- day_11/solve.ts
+++ day_11/solve.ts
@@ -13,11 +13,14 @@ function blink(stone: number): number[] {
     if (stone === 0) {
         return [1];
     }
-    const str_stone = stone.toString();
-    if (str_stone.length%2 === 0) {
-        const left = str_stone.slice(0, str_stone.length/2);
-        const right = str_stone.slice(str_stone.length/2);
-        return [Number(left), Number(right)];
+    
+    // Get number of digits
+    const numDigits = Math.floor(Math.log10(stone)) + 1;    
+    if (numDigits % 2 === 0) {
+        const divisor = 10 ** (numDigits / 2);
+        const right = stone % divisor;
+        const left = Math.floor(stone / divisor);
+        return [left, right];
     }
     return [2024 * stone];
 }
@@ -26,8 +29,16 @@ function blinkAll(stones: number[]): number[] {
     return stones.map(stone => blink(stone)).flat();
 }
 
+// Part 1: 185894
+
 for (let i=0; i<25; i++) {
     data = blinkAll(data);
 }
 
 console.log("Part 1:", data.length);
+
+for (let i=0; i<45; i++) {
+    data = blinkAll(data);
+    console.log(i, data.length);
+}
+console.log("Part 2:", data.length);
