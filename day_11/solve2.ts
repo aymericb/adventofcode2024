a408f27199f102ea01a3718d75c752da3a991417
Aymeric Barthe <aymeric@barthe.ph>
2024-12-11 08:51:13 +0000

    JavaScript is not going to cut it.

--- day_11/solve2.ts
+++ day_11/solve2.ts
@@ -9,6 +9,16 @@ function parseData(text: string): number[] {
 const data = parseData(text);
 // console.log(data);
 
+const memo = new Map<bigint, bigint>();
+
+function blinkMemo(stone: bigint, depth: number, digits?: number): bigint {
+    if (memo.has(stone)) {
+        return memo.get(stone)!;
+    }
+    return blink(stone, depth, digits);
+}
+
+
 function blink(stone: bigint, depth: number, digits?: number): bigint {
     if (depth === 0) {
         return 1n;
@@ -99,16 +109,13 @@ function blinkAllStr(stones: number[], depth: number): bigint {
 
 function createWorker(stone: bigint, depth: number) {
     const workerCode = `
-        // Copy your blinkStr and related functions/constants here
-        const BigIntZero = BigInt(0);
-        const BigIntOne = BigInt(1);
-        const BigInt2024 = BigInt(2024);
-
+        let memo = new Map();
+        ${blinkMemo.toString()}
         ${blink.toString()}
 
         self.onmessage = (e) => {
             const { stone, depth } = e.data;
-            const result = blink(stone, depth);
+            const result = blinkMemo(stone, depth);
             self.postMessage(result);
         };
     `;
@@ -143,6 +150,7 @@ console.log("Part 1:", data.length);
 
 async function blinkAllParallel(stones: number[], depth: number): Promise<bigint> {
     const BATCH_SIZE = navigator.hardwareConcurrency || 4; // Use available CPU cores
+    console.log("BATCH_SIZE:", BATCH_SIZE);
     let tally = 0n;
     const bigStones = stones.map(stone => BigInt(stone));
     
@@ -203,6 +211,16 @@ async function measureTimeAsync(fn: () => Promise<bigint>, label: string = "Oper
     return result;
 }
 
+function blinkAllMemo(stones: number[], depth: number): bigint {
+    const bigStones = stones.map(stone => BigInt(stone));
+    let tally = 0n;
+    for (let i = 0; i < bigStones.length; i++) {
+        // console.log("Debug:", bigStones[i], depth);
+        tally += blinkMemo(bigStones[i], depth);
+    }
+    return tally;
+}
+
 
 
 console.log("Debug:", blinkAll([125, 17], 6));
@@ -210,12 +228,17 @@ console.log("Debug:", blinkAll([125, 17], 6));
 // Part 1: 185894
 console.log("Part 1:", blinkAll(data, 25));
 
-measureTime(() => blinkAll(data, 36), "blnkall 36");
-measureTime(() => blinkAllStr(data, 36), "blnkall str 36");
-// await measureTimeAsync(() => blinkAllParallel(data, 46), "blnkall parallel 46");
+// measureTime(() => blinkAll(data, 46), "blnkall 46");
+// measureTime(() => blinkAllMemo(data, 46), "blnkall memo 46");
+// measureTime(() => blinkAllStr(data, 36), "blnkall str 36");
+// await measureTimeAsync(() => blinkAllParallel(data, 36), "blnkall memo 36");
+// 1.5x factor
+await measureTimeAsync(() => blinkAllParallelRoundArray(data, 45), "blnkall parallel round 45");
 // await measureTimeAsync(() => blinkAllParallelRoundArray(data, 46), "blnkall parallel round 46");
+// await measureTimeAsync(() => blinkAllParallelRoundArray(data, 47), "blnkall parallel round 47");
 
 // console.log("40", blinkAll(data, 40));
 
 
 // console.log("Part 2:", blinkAll(data, 75));
+// 
